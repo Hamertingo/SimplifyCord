c6d22252cc23dc62f2a8b65bdaf5dff38c775684
Hamerti <hamerti63@gmail.com>
2024-12-06 06:43:20 -0300

    Ok

--- src/test/index.ts
+++ src/test/index.ts
@@ -2,6 +2,7 @@ import bootstrapApp from "../class/Client.js";
 
 new bootstrapApp({ 
     autoImport: ["src/test/commands", "src/test/events"],
+    token: "",
     loadLogs: true
     //commands: {
     //    guilds: [""],
